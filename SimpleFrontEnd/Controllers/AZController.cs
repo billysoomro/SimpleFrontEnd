38b4573de6de1e16ce89e8ef80d38a57e8662841
billysoomro <billy.soomro02@gmail.com>
2024-06-27 23:25:22 +0100

    Simplifying AZ controller

---
+++ SimpleFrontEnd/Controllers/AZController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleFrontEnd.Utlilities;
+
+namespace SimpleCrudApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AZController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var availabilityZone = await InstanceMetadataRetriever.GetAvailabilityZoneAsync();
+
+                return Ok(availabilityZone);
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ SimpleFrontEnd/Utilities/InstanceMetadataRetriever.cs
@@ -0,0 +1,53 @@
+﻿namespace SimpleFrontEnd.Utlilities
+{
+    public static class InstanceMetadataRetriever
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private const string InstanceMetadataUrl = "http://169.254.169.254/latest/meta-data/";               
+
+        public static async Task<string> GetAvailabilityZoneAsync()
+        {
+            return await GetMetadataAsync("placement/availability-zone");
+        }
+
+        private static async Task<string> GetMetadataAsync(string path)
+        {
+            try
+            {
+                var token = await GetImdsV2TokenAsync();
+                var metadataUrl = $"http://169.254.169.254/latest/meta-data/{path}";                              
+                var request = new HttpRequestMessage(HttpMethod.Get, metadataUrl);
+
+                request.Headers.Add("X-aws-ec2-metadata-token", token);
+                                
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                                
+                var metadata = await response.Content.ReadAsStringAsync();
+
+                return metadata;
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception($"Error retrieving metadata: {ex.Message}");
+            }
+        }
+
+        private static async Task<string> GetImdsV2TokenAsync()
+        {
+            var tokenUrl = "http://169.254.169.254/latest/api/token";
+            var ttlSeconds = 21600;
+            var request = new HttpRequestMessage(HttpMethod.Put, tokenUrl);
+
+            request.Headers.Add("X-aws-ec2-metadata-token-ttl-seconds", ttlSeconds.ToString());
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var token = await response.Content.ReadAsStringAsync();
+
+            return token;
+        }
+    }
+}
