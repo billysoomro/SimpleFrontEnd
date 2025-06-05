690e0cb6af5e7f55e357c2338ea66c9f4aa960a3
billysoomro <billy.soomro02@gmail.com>
2025-06-05 22:16:47 +0100

    Re-instating the original program file and fixing the functional handler and various lambda stuff

---
+++ SimpleFrontEnd/Program.cs
@@ -0,0 +1,44 @@
+﻿using SimpleFrontEnd.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpClient("SimpleCrudApiClient", client =>
+{
+    client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("SIMPLECRUDAPI_BASE_URL"));
+});
+
+builder.Services.AddHealthChecks().AddCheck<SimpleCrudApiHealthCheck>("SimpleCrudApiHealthCheck>");
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Guitars/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Guitars}/{action=Index}/{id?}");
+
+app.MapHealthChecks("/health");
+
+app.Run();
